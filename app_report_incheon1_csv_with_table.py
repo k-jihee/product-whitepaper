41692d50c8e2561dc9f5181c03e3a0924d90ecf5
k-jihee <foodsafetyin1@gmail.com>
2025-06-02 11:05:54 +0900

    Update app_report_incheon1_csv_with_table.py

--- app_report_incheon1_csv_with_table.py
+++ app_report_incheon1_csv_with_table.py
@@ -164,7 +164,6 @@ if query:
         display: none;
         position: fixed;
         inset: 0;
-        left: 100vw; top: 100vh;
         width: 100%; height: 100%;
         background-color: rgba(0,0,0,0.8);
         z-index: 9999;
