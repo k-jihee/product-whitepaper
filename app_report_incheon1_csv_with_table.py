61fb5c5345ce9216cfb57f2152371d18a871adc6
k-jihee <foodsafetyin1@gmail.com>
2025-11-28 12:43:45 +0900

    Update app_report_incheon1_csv_with_table.py

--- app_report_incheon1_csv_with_table.py
+++ app_report_incheon1_csv_with_table.py
@@ -93,7 +93,7 @@ def show_intro_page():
             /* 🔹 인트로 이미지 크기 & 정렬 */
             .intro-wrap img {
                 width: 100%;             /* 기존 120% → 135% 로 확대 */
-                max-width: 1200px;       /* 최대치도 같이 키움 */
+                max-width: 1000px;       /* 최대치도 같이 키움 */
                 margin-left: auto;
                 margin-right: auto;
                 display: block;
